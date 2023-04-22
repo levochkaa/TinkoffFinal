4302eea8ba79879e2e36715ebb51a82619563919
levochkaa <levochkaaa@icloud.com>
2023-04-22 12:35:50 +0300

    package created, demoapp, removed storyboard

---
+++ Sources/Headers/HeaderView.swift
@@ -0,0 +1,8 @@
+// HeaderView.swift
+
+class HeaderView: UIView {
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ Sources/Module.swift
@@ -0,0 +1,3 @@
+// File.swift
+
+@_exported import UIKit
