d493649a8049ad4397316001ad7bb7c6d7b8eb8e
levochkaa <levochkaaa@icloud.com>
2023-04-22 11:17:40 +0300

    Initial Commit

---
+++ TinkoffFinal/TinkoffFinalApp.swift
@@ -0,0 +1,12 @@
+// TinkoffFinalApp.swift
+
+import SwiftUI
+
+@main
+struct TinkoffFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
